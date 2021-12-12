2e1aa25f2c2a26a2b8494db7559d66bad8111864
Cameron Daly <camerondaly@Camerons-MacBook-Air.local>
2021-12-12 10:26:58 -0500

    Tests look good, ready to submit.

--- module-2/SLLTests.java
+++ module-2/SLLTests.java
@@ -1,6 +1,6 @@
 public class SLLTests {
     public static void main(String[] args) {
-        SinglyLinkedList<Object> myList = new SinglyLinkedList<Object>();
+        SinglyLinkedList<Integer> myList = new SinglyLinkedList<Integer>();
         
         myList.addToBack(0);
         printList(myList);
@@ -17,25 +17,53 @@ public class SLLTests {
         myList = initilializeLongList();
         printList(myList);
 
-        
+        System.out.printf("Removed from back: %d \n", myList.removeFromBack());
+        printList(myList);
+
+        System.out.printf("Removed from back: %d \n", myList.removeFromBack());
+        printList(myList);
+
+        System.out.printf("Removed from back: %d \n", myList.removeFromBack());
+        printList(myList);
+
+        System.out.printf("Removed from back: %d \n", myList.removeFromBack());
+        printList(myList);
+
+        System.out.println("Making another longer list...\n");
+        SinglyLinkedList<Integer> secondList = initilializeLongList();
+
+        System.out.printf("Removed from front: %d \n", secondList.removeFromFront());
+        printList(secondList);
+        System.out.printf("Removed from front: %d \n", secondList.removeFromFront());
+        printList(secondList);
+        System.out.printf("Removed from front: %d \n", secondList.removeFromFront());
+        printList(secondList);
+        System.out.printf("Removed from back: %d \n", secondList.removeFromBack());
+        printList(secondList);
+
     }
 
-    public static void printList(SinglyLinkedList<Object> myList) {
-        SinglyLinkedListNode<Object> curr = myList.getHead();
+    public static void printList(SinglyLinkedList<Integer> myList) {
+        SinglyLinkedListNode<Integer> curr = myList.getHead();
         String listAsText = "";
         while (curr != null) {
-            listAsText = listAsText.concat((String)curr.getData() + " ");
+            listAsText = listAsText.concat(String.valueOf(curr.getData()) + " ");
+            curr = curr.getNext();
+        }
+        if (myList.size() == 0) {
+            System.out.println("The list is empty.\n");
+            return;
         }
         System.out.printf("The list is currently: '%s' \n", listAsText);
-        System.out.printf("The head is: %d \n", myList.getHead());
-        System.out.printf("The tail is: %d \n", myList.getTail());
-        System.out.printf("The size is: %d \n\n", myList.size());
+        System.out.printf("The head is: %s \n", String.valueOf(myList.getHead().getData()));
+        System.out.printf("The tail is: %s \n", String.valueOf(myList.getTail().getData()));
+        System.out.printf("The size is: %s \n\n", String.valueOf(myList.size()));
     }
 
-    public static SinglyLinkedList<Object> initilializeLongList() {
-        SinglyLinkedList<Object> newList = new SinglyLinkedList<Object>();
-        for (int i = 0; i < 6; i++) {
-            newList.addToFront(i);
+    public static SinglyLinkedList<Integer> initilializeLongList() {
+        SinglyLinkedList<Integer> newList = new SinglyLinkedList<Integer>();
+        for (int i = 0; i < 4; i++) {
+            newList.addToBack(i);
         }
         return newList;
     }
